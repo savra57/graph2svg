81660d6f55dbc7324f0eea8e04ca1cb919e369ce
Jakub.Vojtisek@gmail.com <Jakub.Vojtisek@gmail.com@ae46138d-2726-0410-8d72-59eac8977e27>
2012-12-14 16:59:49 +0000

    resolving some DTD validation problems

--- trunk/test/javaTools/src/com/googlecode/graph2svg/tools/jrav/MyErrorHandler.java
+++ trunk/test/javaTools/src/com/googlecode/graph2svg/tools/jrav/MyErrorHandler.java
@@ -1,24 +1,51 @@
 package com.googlecode.graph2svg.tools.jrav;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.xml.sax.ErrorHandler;
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
 
-class MyErrorHandler implements ErrorHandler{
+class MyErrorHandler implements ErrorHandler {
+
+	private List<String> messages = new ArrayList<String>();
+	private String validationType = "";
+
+	public MyErrorHandler(String type) {
+		validationType = type + ": ";
+	}
+
 	public void fatalError(SAXParseException e) throws SAXException {
-		printException("FATAL ERROR", e);
+		addMessage("FATAL ERROR", e);
 	}
 
 	public void error(SAXParseException e) throws SAXException {
-		printException("ERROR", e);
+		addMessage("ERROR", e);
 	}
 
 	public void warning(SAXParseException e) {
-		printException("WARNING", e);
+		addMessage("WARNING", e);
 	}
-	
-	private void printException(String prefix, SAXParseException e) {
-		System.out.print("\n    " + prefix + 
-				" (line: " + e.getLineNumber() + ", col: " + e.getColumnNumber() + "): " + e);
+
+	private void addMessage(String prefix, SAXParseException e) {
+		String message = validationType + prefix + " (line: "
+				+ e.getLineNumber() + ", col: " + e.getColumnNumber() + "): "
+				+ e;
+		messages.add(message);
+	}
+
+	public void reset() {
+		messages.clear();
+	}
+
+	public void printMessages() {
+		for (String message : messages) {
+			System.out.println("   " + message);
+		}
+	}
+
+	public boolean hasMessages() {
+		return !messages.isEmpty();
 	}
 }
