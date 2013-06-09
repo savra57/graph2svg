9fec816ad1c3127e4870552f3aecc45e27967220
Jakub.Vojtisek@gmail.com <Jakub.Vojtisek@gmail.com@ae46138d-2726-0410-8d72-59eac8977e27>
2013-06-09 19:33:52 +0000

    print relax NG validation exceptions

--- trunk/test/javaTools/src/com/googlecode/graph2svg/tools/jrav/BatchValidator.java
+++ trunk/test/javaTools/src/com/googlecode/graph2svg/tools/jrav/BatchValidator.java
@@ -17,59 +17,64 @@ import com.googlecode.graph2svg.tools.fileFilter.RegExpFileFilter;
  */
 public class BatchValidator {
 
-	public static void main(String[] args)
-			throws VerifierConfigurationException, SAXException, IOException {
-		// create a VerifierFactory
-		VerifierFactory factory = new com.sun.msv.verifier.jarv.TheFactoryImpl();
+	public static void main(String[] args) throws VerifierConfigurationException, SAXException, IOException {
+		try
+		{
+			// create a VerifierFactory
+			VerifierFactory factory = new com.sun.msv.verifier.jarv.TheFactoryImpl();
 
-		if (args.length < 2) {
-			printUsage();
-			return;
-		}
-
-		String schemaFileName = args[0]; // "xygr.rng" or xygr.dtd;
-		
-		// compile a RELAX schema (or whatever schema you like)
-		File schemaFile = new File(schemaFileName);
-		Schema schema = factory.compileSchema(schemaFile);
-
-		// obtain a verifier
-		Verifier verifier = schema.newVerifier();
-		MyErrorHandler myErrorHandler = new MyErrorHandler(schemaFile.getName());
-		verifier.setErrorHandler(myErrorHandler);
-
-		String fileName = args[1]; // the file to validate or a directory with xml files
-
-		File file = new File(fileName);
-
-		if (file.isDirectory()) {
-			String filterString;
-			if (args.length >= 3) {
-				filterString = args[2];
-			} else {
-				filterString = ".*\\.xml"; // default
+			if (args.length < 2) {
+				printUsage();
+				return;
 			}
-			File[] fileList = file
-					.listFiles(new RegExpFileFilter(filterString));
-			for (File f : fileList) {
-				
-				// this has to be called because of some bug in the veryfier causing that error messages of
-				// a second invalid XML file in a row are not recored
-				verifier = schema.newVerifier();
-				verifier.setErrorHandler(myErrorHandler);
-				
-				myErrorHandler.reset();
-				verifyFile(verifier, f);
+
+			String schemaFileName = args[0]; // "xygr.rng" or xygr.dtd;
+
+			// compile a RELAX schema (or whatever schema you like)
+			File schemaFile = new File(schemaFileName);
+			Schema schema = factory.compileSchema(schemaFile);
+
+			// obtain a verifier
+			Verifier verifier = schema.newVerifier();
+			MyErrorHandler myErrorHandler = new MyErrorHandler(schemaFile.getName());
+			verifier.setErrorHandler(myErrorHandler);
+
+			// the file to validate or a directory with xml files
+			String fileName = args[1];
+
+			File file = new File(fileName);
+
+			if (file.isDirectory()) {
+				String filterString;
+				if (args.length >= 3) {
+					filterString = args[2];
+				} else {
+					filterString = ".*\\.xml"; // default
+				}
+				File[] fileList = file.listFiles(new RegExpFileFilter(filterString));
+				for (File f : fileList) {
+
+					// this has to be called because of some bug in the verifier
+					// causing that error messages of a second invalid XML file
+					// in a row are not recored
+					verifier = schema.newVerifier();
+					verifier.setErrorHandler(myErrorHandler);
+
+					myErrorHandler.reset();
+					verifyFile(verifier, f);
+					myErrorHandler.printMessages();
+				}
+			} else {
+				verifyFile(verifier, file);
 				myErrorHandler.printMessages();
 			}
-		} else {
-			verifyFile(verifier, file);
-			myErrorHandler.printMessages();
+
+		} catch (Exception e) {
+			System.out.println("Exception running validator: " + e);
 		}
 	}
 
-	private static void verifyFile(Verifier verifier, File xmlFile)
-			throws SAXException, IOException {
+	private static void verifyFile(Verifier verifier, File xmlFile) throws SAXException, IOException {
 		System.out.print(xmlFile + ": ");
 		try {
 			if (verifier.verify(xmlFile)) {
